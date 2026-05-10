3750ce2b4e28e76a8a926375d9ba35dff890793e
Nathan Huh <nathanhuh@lguplus.co.kr>
2026-05-10 10:19:59 +0900

    57E-1 Finish the first exercise for 57 exercises book

--- rust/57-exercises/p1/src/main.rs
+++ rust/57-exercises/p1/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    print!("What is your name?: ");
+    io::stdout().flush().expect("flush failed");
+
+    let mut name_input = String::new();
+    io::stdin()
+        .read_line(&mut name_input)
+        .expect("Invalid name");
+    name_input = name_input.trim().to_string();
+
+    let result = "Hello, ".to_owned() + &name_input + ", nice to meet you!";
+
+    println!("{}", result);
 }
