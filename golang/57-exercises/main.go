5b87cbf403e2ef50eb7cc4d1a106153df9e0f630
Nathan Huh <nathanhuh@lguplus.co.kr>
2026-05-10 09:42:11 +0900

    chore: commit all local artifacts i've worked on work laptop to git repo

---
+++ golang/57-exercises/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"57-exercises/exercises"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go Exercises")
+	fmt.Println("============")
+
+	fmt.Println("\nExercise 1: ")
+}
--- rust/rust-lang-book/guessing_name/src/main.rs
+++ rust/rust-lang-book/guessing_name/src/main.rs
@@ -7,7 +7,9 @@ fn main() {
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // Break'em into multiple lines for readability
 
     println!("You guessed: {guess}");
 }
